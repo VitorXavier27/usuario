c881e88491d1a2be4d0cc11e6f1bcd55a3dbfb3f
Vitor Hugo Silva Xavier <vitor.xavier@venture.local>
2026-07-28 10:56:50 -0300

    adicionado classes de security

---
+++ src/main/java/com/xavier/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,7 @@
+package com.xavier.usuario.infrastructure.repository;
+
+import com.xavier.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
