f5cc54e331bcf1c0e0c077f2341437e5c9131892
VitorXavier27 <vitorxa012@gmail.com>
2026-07-29 10:16:27 -0300

    inclusão do metodo put usuario

--- src/main/java/com/xavier/usuario/business/UsuarioService.java
+++ src/main/java/com/xavier/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.xavier.usuario.infrastructure.entity.Usuario;
 import com.xavier.usuario.infrastructure.exceptions.ConflictException;
 import com.xavier.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.xavier.usuario.infrastructure.repository.UsuarioRepository;
+import com.xavier.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +19,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvarUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -52,5 +54,21 @@ public class UsuarioService {
 
     public void deletarUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token,UsuarioDTO dto){
+        // busca email atraves do token
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+
+        // criptografia senha
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        // busca usuario no banco de dados
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotFoundException("Email não localizado"));
+        // mesclagem de dados com o dto e o banco de dados para verificar a busca
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+        // salvou os dados no banco de dados e depois converte para usuario dto
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 }
--- src/main/java/com/xavier/usuario/controller/UsuarioController.java
+++ src/main/java/com/xavier/usuario/controller/UsuarioController.java
@@ -45,4 +45,10 @@ public class UsuarioController {
         usuarioService.deletarUsuarioPorEmail(email);
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping
+    public ResponseEntity<UsuarioDTO> atualizarDadoUsuario(@RequestBody UsuarioDTO dto,
+                                                           @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token, dto));
+    }
 }
